8819c399d91f45f40076915393bc2e4a79fb5b4b
drdrej <andrej.siebert@googlemail.com>
2015-07-22 21:16:03 +0200

    change Owner stuff

---
+++ app/src/main/java/com/touchableheroes/drafts/behaviours/config/IOwnerAssignable.java
@@ -0,0 +1,10 @@
+package com.touchableheroes.drafts.behaviours.config;
+
+/**
+ * Created by asiebert on 22.07.15.
+ */
+public interface IOwnerAssignable<T> {
+
+    public void assignOwner(T owner);
+
+}
